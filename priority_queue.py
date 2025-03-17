28b000f38b2cea8c69ffe9a6fe3500f138bad3c8
tlawjddnjs <157342006+tlawjddnjs@users.noreply.github.com>
2025-03-17 17:29:18 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -14,7 +14,7 @@ class PriorityQueue:
       self._sift_down(0)
       return item
     elif len(self.heap) == 1:
-      priority, item = self.heap.pop()
+      _, item = self.heap.pop()
       return item
     else:
       return None
